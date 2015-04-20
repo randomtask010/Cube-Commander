a60f615bc4b8171473c166f4580dd0b844e2b5a3
Bio-Hazzard <coolcool0987@hotmail.com>
2015-04-20 17:44:47 +1000

    Synced assets with repository

---
+++ cube-commander-unity-4/Assets/scripts/sound.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class sound : MonoBehaviour {
+
+	public AudioClip chime;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void chimePlay () {
+		audio.PlayOneShot (chime);
+	}
+
+}
